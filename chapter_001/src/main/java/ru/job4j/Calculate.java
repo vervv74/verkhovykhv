ce04fda3af1ce508b124265d2d52ac432ca51e42
vervv74 <vervv74@gmail.com>
2017-10-15 15:03:33 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,17 @@
+/**
+* First class
+*/
  package ru.job4j; 
+/**
+* @author Valentin Verkhovykh (mailto:vervv74@gmail.com)
+* @since 15.10.17
+*/
  public class Calculate{
+/**
+* @author Valentin Verkhovykh (mailto:vervv74@gmail.com)
+* @param массив String[] args для запуска методов на исполнение
+* @since 15.10.17
+*/
 	 public static void main(String[] args){
 		 System.out.println("Hello world");
 	 }
